d6c2d332038400df40e496d162910e4363b5f302
anaritasilva92 <anaritasilva.92@hotmail.com>
2025-11-24 23:17:00 +0000

    Create basedados.py

---
+++ basedados.py
@@ -0,0 +1,11 @@
+# Deve ligar a um servidor de base dados
+
+import sys
+
+servidor= sys.argv[1]
+utilizador= sys.argv[2]
+password= sys.argv[3]
+
+print("O servidor é: ", servidor)
+print("O utilizador é: ", utilizador)
+print("A password é: ", password)
